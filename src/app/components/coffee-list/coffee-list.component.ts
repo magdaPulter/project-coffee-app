40417a734edefa029a61c57ece2935b8de3b473c
magdaPulter <magda.pulter@gmail.com>
2023-11-25 18:37:45 +0100

    coffee list created

---
+++ src/app/components/coffee-list/coffee-list.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-coffee-list',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './coffee-list.component.html',
+  styleUrls: ['./coffee-list.component.scss']
+})
+export class CoffeeListComponent {
+
+}

b5ec994b550e5d0e6cad40f74ca4d97bf80a41c1
magdaPulter <magda.pulter@gmail.com>
2023-11-26 11:18:39 +0100

    routing added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,8 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DetailComponent } from './components/detail/detail.component';
 
-const routes: Routes = [];
+const routes: Routes = [{ path: 'coffee/:id', component: DetailComponent }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],

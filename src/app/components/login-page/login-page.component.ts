e45d72317bf8bc7eb2a86307c768d7a1ed446b02
magdaPulter <magda.pulter@gmail.com>
2024-02-12 12:16:50 +0100

    password validation added

--- src/app/components/login-page/login-page.component.ts
+++ src/app/components/login-page/login-page.component.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import {
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { BehaviorSubject, Observable } from 'rxjs';
+import { RegisterService } from '../../services/register.service';
+import { matchPassword } from 'src/app/utils/match-password';
 
 @Component({
   selector: 'app-login-page',
@@ -21,23 +28,39 @@ import { BehaviorSubject, Observable } from 'rxjs';
 })
 export class LoginPageComponent {
   readonly loginForm: FormGroup = new FormGroup({
-    email: new FormControl(),
-    password: new FormControl(),
+    email: new FormControl(null, Validators.email),
+    password: new FormControl(null, [Validators.required]),
   });
-  readonly registerForm: FormGroup = new FormGroup({
-    email: new FormControl(),
-    password: new FormControl(),
-    confirmPassword: new FormControl(),
-  });
+  readonly registerForm: FormGroup = new FormGroup(
+    {
+      email: new FormControl(null, Validators.email),
+      password: new FormControl(null, [Validators.required]),
+      confirmPassword: new FormControl(null, [Validators.required]),
+    },
+    {
+      validators: [matchPassword],
+    }
+  );
   private _isRegisteredSubject: BehaviorSubject<boolean> =
     new BehaviorSubject<boolean>(true);
   public isRegistered$: Observable<boolean> =
     this._isRegisteredSubject.asObservable();
+
+  constructor(private _registerService: RegisterService) {}
 
   onRegister(isRegistered: boolean) {
     this._isRegisteredSubject.next(isRegistered);
   }
 
   onLoginFormSubmitted(loginForm: FormGroup): void {}
-  onregisterFormSubmitted(loginForm: FormGroup): void {}
+
+  onregisterFormSubmitted(registerForm: FormGroup): void {
+    if (registerForm.valid) {
+      this._registerService.create({
+        email: registerForm.get('email')!.value,
+        password: registerForm.get('password')!.value,
+        confirmPassword: registerForm.get('confirmPassword')!.value,
+      });
+    }
+  }
 }
--- src/app/models/sign-up.model.ts
+++ src/app/models/sign-up.model.ts
@@ -1,4 +1,5 @@
 export interface SignUpModel {
   readonly email: string;
   readonly password: string;
+  readonly confirmPassword: string;
 }

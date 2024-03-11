eabb4d74dc1016775d02dceccd20ae366c4bcc7e
magdaPulter <magda.pulter@gmail.com>
2024-03-11 15:18:59 +0100

    logic added to the inventory-summary component

--- src/app/components/inventory-summary/inventory-summary.component.ts
+++ src/app/components/inventory-summary/inventory-summary.component.ts
@@ -1,13 +1,27 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import {
+  AfterViewInit,
+  Component,
+  EventEmitter,
+  Input,
+  OnChanges,
+  OnInit,
+  Output,
+  SimpleChanges,
+} from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { BehaviorSubject, Observable, Subject } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { CoffeeWithUrlQueryModel } from '../../querymodels/coffeeWithUrl.querymodel';
+import { CoffeeModel } from '../../models/coffee.model';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import {
   faBagShopping,
   faChartLine,
   faFolderClosed,
-  faTriangleExclamation,
   faPercent,
+  faTriangleExclamation,
 } from '@fortawesome/free-solid-svg-icons';
-import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+
 @Component({
   selector: 'app-inventory-summary',
   standalone: true,
@@ -15,7 +29,8 @@ import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
   templateUrl: './inventory-summary.component.html',
   styleUrls: ['./inventory-summary.component.scss'],
 })
-export class InventorySummaryComponent {
+export class InventorySummaryComponent implements OnChanges {
+  @Input() coffeeList!: CoffeeWithUrlQueryModel[];
   @Output() productAdded: EventEmitter<void> = new EventEmitter<void>();
 
   faBagShopping = faBagShopping;
@@ -23,6 +38,40 @@ export class InventorySummaryComponent {
   faFolderClosed = faFolderClosed;
   faTriangleExclamation = faTriangleExclamation;
   faPercent = faPercent;
+
+  private _currentCoffeeListSubject: BehaviorSubject<
+    CoffeeWithUrlQueryModel[]
+  > = new BehaviorSubject<CoffeeWithUrlQueryModel[]>(this.coffeeList);
+  public currentCoffeeList$: Observable<CoffeeWithUrlQueryModel[]> =
+    this._currentCoffeeListSubject.asObservable();
+
+  ngOnChanges(changes: SimpleChanges): void {
+    this._currentCoffeeListSubject.next(changes['coffeeList'].currentValue);
+  }
+
+  readonly unPublished$: Observable<number> = this.currentCoffeeList$.pipe(
+    map((list) => {
+      return list.filter((product) => {
+        return product.unPublished;
+      }).length;
+    })
+  );
+
+  readonly lowStock$: Observable<number> = this.currentCoffeeList$.pipe(
+    map((list) => {
+      return list.filter((product) => {
+        return +product.quantity < 5;
+      }).length;
+    })
+  );
+
+  readonly discounted$: Observable<number> = this.currentCoffeeList$.pipe(
+    map((list) => {
+      return list.filter((product) => {
+        return +product.discount > 0;
+      }).length;
+    })
+  );
 
   addNewProduct() {
     this.productAdded.emit();

3d3912cb5a3981023505fd4559f35e643866cf31
magdaPulter <magda.pulter@gmail.com>
2024-04-06 12:48:54 +0200

    fontello icons added

--- src/app/components/inventory-summary/inventory-summary.component.ts
+++ src/app/components/inventory-summary/inventory-summary.component.ts
@@ -1,41 +1,18 @@
-import {
-  Component,
-  EventEmitter,
-  Input,
-  OnChanges,
-  Output,
-  SimpleChanges,
-} from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { BehaviorSubject, Observable, Subject } from 'rxjs';
-import { map } from 'rxjs/operators';
+import { BehaviorSubject, Observable } from 'rxjs';
 import { CoffeeWithUrlQueryModel } from '../../querymodels/coffeeWithUrl.querymodel';
-import { CoffeeModel } from '../../models/coffee.model';
-import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-import {
-  faBagShopping,
-  faChartLine,
-  faFolderClosed,
-  faPercent,
-  faTriangleExclamation,
-} from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-inventory-summary',
   standalone: true,
-  imports: [CommonModule, FontAwesomeModule],
+  imports: [CommonModule],
   templateUrl: './inventory-summary.component.html',
   styleUrls: ['./inventory-summary.component.scss'],
 })
 export class InventorySummaryComponent {
   @Input() coffeeList!: CoffeeWithUrlQueryModel[];
   @Output() productAdded: EventEmitter<void> = new EventEmitter<void>();
-
-  faBagShopping = faBagShopping;
-  faChartLine = faChartLine;
-  faFolderClosed = faFolderClosed;
-  faTriangleExclamation = faTriangleExclamation;
-  faPercent = faPercent;
 
   private _currentCoffeeListSubject: BehaviorSubject<
     CoffeeWithUrlQueryModel[]
